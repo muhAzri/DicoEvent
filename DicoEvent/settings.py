391631491ca25dfb4d17a9c20f66bea8689647ce
Muhammad Azri Fatihah Susanto <Muhammad.azri.f.s@gmail.com>
2025-08-02 08:54:45 +0700

    feat: update database environment variables to match required naming convention

--- DicoEvent/settings.py
+++ DicoEvent/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'DicoEvent.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
