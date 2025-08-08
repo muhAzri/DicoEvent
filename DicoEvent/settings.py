bd9af9a747b1f68e0c42c2f561703053b63d0ca4
Muhammad Azri Fatihah Susanto <Muhammad.azri.f.s@gmail.com>
2025-08-08 10:44:04 +0700

    fix: celery

--- DicoEvent/settings.py
+++ DicoEvent/settings.py
@@ -184,9 +184,10 @@ CACHES = {
 # Cache timeout (1 hour = 3600 seconds)
 CACHE_TTL = 3600
 
-# Celery Configuration
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+# Celery Configuration  
+REDIS_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
